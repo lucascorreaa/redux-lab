a143681a96abba46f24caa2d45e83b3dc9d03a8a
lucascorreaa <lucas.lca14@gmail.com>
2025-01-06 00:07:06 -0300

    feat: added function async with redux

--- src/state/counter/counterSlice.ts
+++ src/state/counter/counterSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit"
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit"
 
 interface CounterState {
   value: number
@@ -15,8 +15,21 @@ const CounterSlice = createSlice({
     increment: (state) => {state.value += 1},
     decrement: (state) => {state.value -= 1},
     incrementByAmount: (state, action: PayloadAction<number>) => {state.value += action.payload}
+  },
+  extraReducers: (builder) => {
+    builder.addCase(incrementAsync.fulfilled, (state, action: PayloadAction<number>) => {
+      state.value += action.payload
+    })
   }
 })
+// exemplo para caso tenha consumo de API
+export const incrementAsync = createAsyncThunk(
+  'counter/incrementAsync',
+  async (amount: number) => {
+    await new Promise((resolve) => setTimeout(resolve, 1000))
+    return amount
+  }
+)
 
 export const { increment, decrement, incrementByAmount } = CounterSlice.actions
 
